a1c503c5cc265ef8743a5c82a37ee84a7487b274
rprovilij <81406457+rprovilij@users.noreply.github.com>
2022-01-18 11:55:12 +0100

    Update spectrogram.py

--- spectrogram.py
+++ spectrogram.py
@@ -8,18 +8,18 @@ import gc
 import os
 
 # AUDIO-IN
-FORMAT = pyaudio.paInt16
-CHANNELS = 2
-RATE = 44100
-CHUNK = 1024
-RECORD_SECONDS = 2
-WAV_AUDIO = "audio.wav"
+FORMAT 		= pyaudio.paInt16
+CHANNELS 	= 2
+RATE 		= 44100
+CHUNK 		= 1024
+RECORD_SECONDS 	= 2
+WAV_AUDIO 	= "audio.wav"
 
 audio = pyaudio.PyAudio()
 
-# start Recording
+# Start recording
 stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
-print("recording...")
+print("> Recording...")
 frames = []
 
 for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
@@ -27,7 +27,7 @@ for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
 	frames.append(data)
 print("finished recording")
 
-# Stop Recording
+# Stop recording
 stream.stop_stream()
 stream.close()
 audio.terminate()
